16c441eb8082c7abca8e076dcb9988391218b3cb
Eric Douglas <eric.douglas.mail@gmail.com>
2019-08-28 08:21:59 -0300

    add: onli-reducer helper

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const onliReducer = (dispatch, types) => {
+  const newDispatch = type => action => dispatch({ type, ...action })
+  const send = types.reduce(
+    (acc, next) => ({ ...acc, [next]: newDispatch(next) }),
+    {}
+  )
+
+  return send
+}
+
+export default onliReducer
