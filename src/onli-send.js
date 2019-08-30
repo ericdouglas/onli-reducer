ad442ba462984d9a5896d33cabb67dccd71bea98
Eric Douglas <eric.douglas.mail@gmail.com>
2019-08-30 06:35:42 -0300

    add: improve onliSend to pass send as a property

--- src/onli-send.js
+++ src/onli-send.js
@@ -1,9 +1,8 @@
 export default (dispatch, types) => {
-  const newDispatch = type => action => dispatch({ type, ...action })
-  const send = types.reduce(
-    (acc, next) => ({ ...acc, [next]: newDispatch(next) }),
-    {}
-  )
+  const send = types.reduce((acc, next) => ({ ...acc, [next]: "" }), {})
+  const newDispatch = type => action => dispatch({ type, send, ...action })
+
+  types.forEach(type => (send[type] = newDispatch(type)))
 
   return send
 }
